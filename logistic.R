ef62db277c87a5f57950f3a7934e098cebe79b78
Shen Lim <yushen-lim@uiowa.edu>
2019-04-26 12:54:41 -0500

    Fixed code indentation

--- logistic.R
+++ logistic.R
@@ -15,20 +15,20 @@ srcRgdp <- read.csv('county_rgdp.csv', stringsAsFactors = FALSE, check.names = F
 
 ## 2. Presidential Election Data Cleanup
 pres <- select(srcPresident, state = state_abbr, county = county_name,
-							 fips = combined_fips,
-							 votesDem12 = votes_dem_2012, votesRep12 = votes_gop_2012,
-							 votesDem16 = votes_dem_2016, votesRep16 = votes_gop_2016) %>%
-	mutate(county = gsub(' County', '', county))
+               fips = combined_fips,
+               votesDem12 = votes_dem_2012, votesRep12 = votes_gop_2012,
+               votesDem16 = votes_dem_2016, votesRep16 = votes_gop_2016) %>%
+  mutate(county = gsub(' County', '', county))
 
 ## 3. Extract Demographic Data in Select States
 demogrSomeF <- function(...) {
-	states <- gsub('\"', '', toupper(sapply(substitute(list(...)), deparse)[-1]))
+  states <- gsub('\"', '', toupper(sapply(substitute(list(...)), deparse)[-1]))
 	
 	demogrSome <- filter(srcDemogr, state_abbreviation %in% states) %>%
-		select(state = state_abbreviation, county = area_name, pop14 = PST045214,
-					 income = INC110213, education = EDU685213, density = POP060210,
-					 white = RHI825214, hispanic = RHI725214, household = HSD310213) %>%
-		mutate(county = gsub(' County', '', county))
+	  select(state = state_abbreviation, county = area_name, pop14 = PST045214,
+	         income = INC110213, education = EDU685213, density = POP060210,
+	         white = RHI825214, hispanic = RHI725214, household = HSD310213) %>%
+	  mutate(county = gsub(' County', '', county))
 	
 	assign('demogrSome', demogrSome, envir = globalenv())
 }
@@ -44,11 +44,11 @@ demogrSome <- inner_join(demogrSome, srcRgdp, by = c('state', 'county'))
 demogrSome$rgdppc14 <- demogrSome$rgdp2014 / demogrSome$pop14
 
 demogrSome$rgdpDelta <- (demogrSome$rgdp2015 - demogrSome$rgdp2012) /
-	demogrSome$rgdp2012
+  demogrSome$rgdp2012
 
 # Define classes: 1 if Republicans win in 2016, 0 if Democrats
 demogrSome$winner16 <- ifelse(demogrSome$votesRep16 > demogrSome$votesDem16, 1 ,0) %>%
-	factor(levels = c(0, 1))
+  factor(levels = c(0, 1))
 
 ## 4. Partition the Dataset
 rm(trainDataIndex, trainData, testData, upData, logReg)
@@ -64,7 +64,7 @@ table(demogrSome$winner16)
 table(trainData$winner16)
 
 upData <- upSample(x = trainData[!(names(trainData) %in% c('winner16'))],
-									 y = trainData$winner16)
+                   y = trainData$winner16)
 
 ## 5. Logistic Regression Model
 logReg <- glm(Class ~ income + education, family = 'binomial', data = upData)
@@ -81,7 +81,7 @@ mean(winPredictions == testData$winner16)
 with(logReg, null.deviance - deviance)
 with(logReg, df.null - df.residual)
 with(logReg, pchisq(null.deviance - deviance, df.null - df.residual,
-										lower.tail = FALSE))
+                    lower.tail = FALSE))
 
 ## 6. Visualization
 # State names in 'choroplethr' must be formatted correctly
@@ -89,30 +89,30 @@ states <- c('illinois', 'iowa', 'michigan', 'minnesota', 'nebraska')
 
 # Plot actual 2016 results
 mapDf <- data.frame(region = demogrSome$fips,
-										value = ifelse(demogrSome$winner16 == 1, 'Republican', 'Democrat') %>%
-											as.factor())
+                    value = ifelse(demogrSome$winner16 == 1, 'Republican', 'Democrat') %>%
+                      as.factor())
 
 county_choropleth(mapDf, state_zoom = states, title = '2016 Actual Results') +
-	scale_fill_manual(values = alpha(c('blue', 'red'), 0.6),
-										labels = c('Democrat', 'Republican'),
-										name = 'Party')
+  scale_fill_manual(values = alpha(c('blue', 'red'), 0.6),
+                    labels = c('Democrat', 'Republican'),
+                    name = 'Party')
 
 # Plot predicted results based on 'education' and 'income'
 predictDf <- data.frame(region = demogrSome$fips,
-												value = ifelse(predict(logReg, newdata = demogrSome, type = 'response') > cutoff,
-																			 'Republican', 'Democrat') %>% as.factor())
+                        value = ifelse(predict(logReg, newdata = demogrSome, type = 'response') > cutoff,
+                                       'Republican', 'Democrat') %>% as.factor())
 
 county_choropleth(predictDf, state_zoom = states, title = '2016 Predictions') +
 	scale_fill_manual(values = alpha(c('blue', 'red'), 0.6),
-										labels = c('Democrat', 'Republican'),
-										name = 'Party')
+	                  labels = c('Democrat', 'Republican'),
+	                  name = 'Party')
 
 # Plot accuracy
 accuracyDf <- data.frame(region = demogrSome$fips,
-												 value = ifelse(mapDf$value == predictDf$value, 'Correct', 'Wrong') %>%
-												 	as.factor())
+                         value = ifelse(mapDf$value == predictDf$value, 'Correct', 'Wrong') %>%
+                           as.factor())
 
 county_choropleth(accuracyDf, state_zoom = states, title = 'Model Accuracy') +
-	scale_fill_manual(values = alpha(c('green', 'red'), 0.6),
-										labels = c('Correct', 'Wrong'),
-										name = 'Result')
+  scale_fill_manual(values = alpha(c('green', 'red'), 0.6),
+                    labels = c('Correct', 'Wrong'),
+                    name = 'Result')
